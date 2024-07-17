f6e4d25469fffc0e466ee6ecb5d42d79ffa1a55d
Mike Neilens <mike.neilens@gmail.com>
2024-07-17 10:58:13 +0100

    july 16 solution for all numbers

--- src/main/kotlin/july16/Problem.kt
+++ src/main/kotlin/july16/Problem.kt
@@ -4,8 +4,12 @@
 //
 //Bonus: Can you do this in one pass?
 
-fun numbersAddUpToK(numbers:List<Int>, k:Int) = true
-
-val foundNumbers = mutableSetOf<Int>()
+fun numbersAddUpToK(numbers:List<Int>, k:Int, foundNumbers:MutableSet<Int> = mutableSetOf()):Boolean {
+    numbers.forEach { n ->
+        if (targetFound(foundNumbers, n, k)) return true
+        else foundNumbers.add(n)
+    }
+    return false
+}
 
 fun targetFound(foundNumbers:Set<Int>, n:Int, k:Int ) = foundNumbers.contains(k - n)
--- src/test/kotlin/july16/ProblemTest.kt
+++ src/test/kotlin/july16/ProblemTest.kt
@@ -1,6 +1,5 @@
 package july16
 
-import foundNumbers
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import numbersAddUpToK
@@ -23,5 +22,14 @@ class ProblemTest: StringSpec( {
     "given a set containing 4 and number 3 and target of 7, target is not found in set" {
         val foundNumbers = setOf<Int>(4)
         targetFound(foundNumbers = foundNumbers, n = 3, k = 7) shouldBe true
+    }
+    "given [11, 15, 3, 7] and k of 17, return false since no two numbers add up to 17" {
+        numbersAddUpToK(numbers = listOf(11,15,3,7), k = 17) shouldBe false
+    }
+    "given [11, 15, 3, 11, 1, 2] and k of 22, return true since 11 + 11 add up to 22" {
+        numbersAddUpToK(numbers = listOf(11,15,3,11,1,2), k = 22) shouldBe true
+    }
+    "given [11, 15, 3, 11,1, 2] and k of 12, return true since 11 + 1 add up to 12" {
+        numbersAddUpToK(numbers = listOf(11,15,11,1,2), k = 12) shouldBe true
     }
 })
