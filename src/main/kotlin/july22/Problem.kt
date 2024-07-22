74f126153c1c3caefc886c1e228f79bf99c4913f
Mike Neilens <mike.neilens@gmail.com>
2024-07-22 18:13:12 +0100

    july 22

--- src/main/kotlin/july22/Problem.kt
+++ src/main/kotlin/july22/Problem.kt
@@ -6,18 +6,13 @@ data class Message(val decodedMessage:String, val encodedMessage:String ) {
             Message(decodedMessage + map.getValue(encodedMessage.take(length)), encodedMessage.drop(length) )
         } else null
 
-    fun decoded():List<Message> {
-        if (encodedMessage.isEmpty()) return listOf(this)
-        val message1 = decodeDigit(length = 1)
-        val message2 = decodeDigit(length = 2)
-        return when {
-            message1 != null && message2 != null -> listOf(message1, message2)
-            message1 != null && message2 == null -> listOf(message1)
-            message1 == null && message2 != null -> listOf(message2)
-            else -> listOf()
+    fun decoded():List<Message> =
+        if (encodedMessage.isEmpty()) listOf(this)
+        else (1..2).fold(emptyList()){ result, length ->
+            val newMessage= decodeDigit(length)
+            if (newMessage!= null) result + newMessage else result
         }
     }
-}
 
 fun decodedAllMessages(messages:List<Message>):List<Message> =
     if (messages.all{it.encodedMessage.isEmpty()})
