c81a85b1fe3a066bfad499c661d44f94734b79ca
Mike Neilens <mike.neilens@gmail.com>
2024-07-24 22:13:26 +0100

    july 24 partially complete

---
+++ src/main/kotlin/july24/Problem.kt
@@ -0,0 +1,26 @@
+package july24
+
+//Given a list of integers, write a function that returns the largest sum of non-adjacent numbers. Numbers can be 0 or negative.
+//
+//For example, [2, 4, 6, 2, 5] should return 13, since we pick 2, 6, and 5. [5, 1, 1, 5] should return 10, since we pick 5 and 5.
+//
+//Follow-up: Can you do this in O(N) time and constant space?
+
+
+//For index 0 create list of [index 0, index 2] and [index 0, index 3]
+//For index 1 create list of [index 1, index 3] and [index 1, index 4]
+//Apply this pattern to the last index of each list so [index 0, index 2] becomes [index 0, index 2, index 2 + 2] and [index 0, index 2, index 2 +3]
+
+fun createLists(lists:List<List<Int>> = listOf(listOf(0),listOf(1)), maxIndex:Int):List<List<Int>> {
+    var newLists = mutableListOf<List<Int>>()
+    lists.forEach { list ->
+        if (list.last() +2 <= maxIndex ) {
+            newLists.add(list + (list.last() + 2))
+            if (list.last() +3 <= maxIndex ) newLists.add(list + (list.last() + 3))
+        } else {
+            newLists.add(list)
+        }
+    }
+    return if (newLists.any{it.last() + 2 <= maxIndex}) createLists(newLists, maxIndex)
+    else newLists
+}
---
+++ src/test/kotlin/july24/ProblemTest.kt
@@ -0,0 +1,37 @@
+package july24
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class ProblemTest: StringSpec({
+    "with max index of 1, list of lists is [0],[1]" {
+        createLists(maxIndex = 1) shouldBe listOf(listOf(0),listOf(1))
+    }
+    "with max index of 2, list of lists is [0,2],[1]" {
+        createLists(maxIndex = 2) shouldBe listOf(listOf(0,2),listOf(1))
+    }
+    "with max index of 3, list of lists is [0,2],[0,3],[1,3]" {
+        createLists(maxIndex = 3) shouldBe listOf(listOf(0,2),listOf(0,3),listOf(1,3))
+    }
+    "with max index of 4, list of lists is [0,2,4],[0,3],[1,3],[1,4]" {
+        createLists(maxIndex = 4) shouldBe listOf(listOf(0,2,4),listOf(0,3),listOf(1,3),listOf(1,4))
+    }
+    "with max index of 5, list of lists is [0,2,4],[0,2,5],[0,3,5],[1,3,5],[1,4]" {
+        createLists(maxIndex = 5) shouldBe listOf(listOf(0,2,4),listOf(0,2,5),listOf(0,3,5),listOf(1,3,5),listOf(1,4))
+    }
+    "with max index of 6, list of lists is [0,2,4,6],[0,2,5],[0,3,5],[0,3,6],[1,3,5],[1,3,6],[1,4,6]" {
+        createLists(maxIndex = 6) shouldBe listOf(listOf(0,2,4),listOf(0,2,5),listOf(0,3,5),listOf(1,3,5),listOf(1,4))
+    }
+    "with max index of 7, list of lists is [0,2,4,6],[0,2,4,7],[0,2,5,7],[0,3,5,7],[0,3,6],[1,3,5,7],[1,3,6],[1,4,6],[1,4,7]" {
+        createLists(maxIndex = 7) shouldBe listOf(
+            listOf(0,2,4,6),
+            listOf(0,2,4,7),
+            listOf(0,2,5,7),
+            listOf(0,3,5,7),
+            listOf(0,3,6),
+            listOf(1,3,5,7),
+            listOf(1,3,6),
+            listOf(1,4,6),
+            listOf(1,4,7))
+    }
+})
