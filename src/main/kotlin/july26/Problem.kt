d70ba80156b3f89689935ca75ec74e0d795d77ee
Mike Neilens <mike.neilens@gmail.com>
2024-07-26 20:07:21 +0100

    july 26

--- src/main/kotlin/july26/Problem.kt
+++ src/main/kotlin/july26/Problem.kt
@@ -6,9 +6,13 @@ fun createDictionary(animals:List<String>): Map<String, Set<String>> {
     val dictionary = mutableMapOf<String, Set<String>>()
     animals.forEach { animal ->
         val keys = animal.toDictionaryKeys()
-        keys.forEach { key -> if (dictionary.contains(key)) dictionary[key] = dictionary.getValue(key) + animal else dictionary[key] = setOf(animal) }
+        keys.forEach { key -> dictionary.addAnimalForKey(key, animal) }
     }
     return dictionary
+}
+
+fun MutableMap<String, Set<String>>.addAnimalForKey(key: String, animal: String) {
+    if (key in this) this[key] = getValue(key) + animal else this[key] = setOf(animal)
 }
 
 fun String.toDictionaryKeys():List<String> = indices.map{take(it + 1).lowercase()}
