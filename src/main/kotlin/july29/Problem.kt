973f06260ad1e6e8ce1c0125ea84400eceb1c65c
Mike Neilens <mike.neilens@gmail.com>
2024-07-29 19:19:16 +0100

    july 29

--- src/main/kotlin/july29/Problem.kt
+++ src/main/kotlin/july29/Problem.kt
@@ -9,8 +9,8 @@ import kotlin.random.Random
 
 
 //Assume a circle has diameter of 1.0. The centre of the circle is at point (0.5, 0.5).
-//The radius at point (a,b) is squareRoot  of (a^2 + y^2) where a and b are the horizontal and vertical distance from the centre
-
+//The distance point (a,b) is from the center is:
+//     squareRoot of (a^2 + y^2) where a and b are the horizontal and vertical distance from the centre.
 fun pointIsInCircle(x:Double, y:Double) = ((y - 0.5).pow(2) + (x - 0.5).pow(2)).pow(0.5) <= 0.5
 
 fun addPointsToCircle(qtyToAdd:Int):Int {
@@ -22,7 +22,7 @@ fun addPointsToCircle(qtyToAdd:Int):Int {
     return qtyInsideCircle
 }
 
-//On a 1 X 1 square contain a circle with a diameter of 1,  Pi is equal to 4 X (points inside a circle)/(total number of points)
+//On a 1 X 1 square containing a circle with a diameter of 1,  Pi is equal to 4 X (points inside a circle)/(total number of points)
 fun calculatePi(qtyToAdd:Int):Double {
     val qtyInsideCirlce = addPointsToCircle(qtyToAdd)
     return (4.0 * qtyInsideCirlce / qtyToAdd).roundToThreeDigits()
