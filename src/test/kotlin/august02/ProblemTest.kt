3f59b4c9bdb343dce99dc891699790d22778e2f4
Mike Neilens <mike.neilens@gmail.com>
2024-08-02 21:25:18 +0100

    aug 2

--- src/test/kotlin/august02/ProblemTest.kt
+++ src/test/kotlin/august02/ProblemTest.kt
@@ -10,13 +10,13 @@ class ProblemTest: StringSpec( {
     "list with one item should give list of one item" {
         sumOfSubArrays(listOf(4),1) shouldBe listOf(4)
     }
-    "list [1,2] and k of 1 shoiuld give [1,2]" {
+    "list [1,2] and k of 1 should give [1,2]" {
         sumOfSubArrays(listOf(1,2),1) shouldBe listOf(1,2)
     }
-    "list [1,2] and k of 2 shoiuld give [2]" {
+    "list [1,2] and k of 2 should give [2] as max of [1,2] is 2" {
         sumOfSubArrays(listOf(1,2),2) shouldBe listOf(2)
     }
-    "list [1,2,3] and k of 2 shoiuld give [2,3]" {
+    "list [1,2,3] and k of 2 should give [2,3] as max of [1,2] is 2 and max of [2,3] is 3" {
         sumOfSubArrays(listOf(1,2,3),2) shouldBe listOf(2,3)
     }
     "list [10, 5, 2, 7, 8, 7] and k = 3 should give [10, 7, 8, 8]" {
