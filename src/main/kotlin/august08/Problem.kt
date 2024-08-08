f9086abf2ff007aa3e9b43645102b6a17827fd3c
Mike Neilens <mike.neilens@gmail.com>
2024-08-08 19:20:49 +0100

    aug 8

---
+++ src/main/kotlin/august08/Problem.kt
@@ -0,0 +1,41 @@
+package august08
+//Implement locking in a binary tree. A binary tree node can be locked or unlocked only if all of its descendants or ancestors are not locked.
+//
+//Design a binary tree node class with the following methods:
+//
+//is_locked, which returns whether the node is locked
+//lock, which attempts to lock the node. If it cannot be locked, then it should return false. Otherwise, it should lock it and return true.
+//unlock, which unlocks the node. If it cannot be unlocked, then it should return false. Otherwise, it should unlock it and return true.
+//
+// You may augment the node to add parent pointers or any other property you would like.
+// You may assume the class is used in a single-threaded program, so there is no need for actual locks or mutexes.
+// Each method should run in O(h), where h is the height of the tree.
+
+enum class Status {Locked, Unlocked}
+
+data class Node(val id:Int, var status:Status = Status.Unlocked, val ancestor:Node? = null, val left:Node? = null, val right:Node? = null) {
+    private fun isLocked() = status == Status.Locked
+
+    fun lock() = changeStatus(Status.Locked)
+
+    fun unlock() = changeStatus(Status.Unlocked)
+
+    private fun changeStatus(newStatus:Status) = if (ancestorsAndDescendantsAreLocked())  {
+        status = newStatus
+        true
+    } else false
+
+    fun ancestorsAndDescendantsAreLocked() = ancestorsAreLocked() && descendantsAreLocked()
+
+    fun ancestorsAreLocked():Boolean = relativesAreLocked(ancestor, Node::ancestorsAreLocked)
+
+    private fun descendantsAreLocked() = leftIsLocked() && rightIsLocked()
+
+    fun leftIsLocked():Boolean = relativesAreLocked(left, Node::descendantsAreLocked)
+
+    fun rightIsLocked():Boolean = relativesAreLocked(right, Node::descendantsAreLocked)
+
+    private fun relativesAreLocked(node:Node?, relativesChecker:Node.()->Boolean) =
+        (node?.isLocked() ?: true) && (node?.relativesChecker() ?: true)
+
+}
---
+++ src/test/kotlin/august08/ProblemTest.kt
@@ -0,0 +1,91 @@
+package august08
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class ProblemTest: StringSpec( {
+    "when a node has no ancestors its ancestors are locked" {
+        val node = Node(1, ancestor = null)
+        node.ancestorsAreLocked() shouldBe true
+    }
+    "when a node has an ancestor which is locked then its ancestors are locked" {
+        val node = Node(1, ancestor = Node(2, status = Status.Locked))
+        node.ancestorsAreLocked() shouldBe true
+    }
+    "when a node has an ancestor which is unlocked then its ancestors are not locked" {
+        val node = Node(1, ancestor = Node(2, status = Status.Unlocked))
+        node.ancestorsAreLocked() shouldBe false
+    }
+    "when a node has an ancestor which is locked which also has an ancestor which is locked then its ancestors are locked" {
+        val node = Node(1, ancestor = Node(2, status = Status.Locked, ancestor = Node(3, status = Status.Locked)))
+        node.ancestorsAreLocked() shouldBe true
+    }
+    "when a node has an ancestor which is locked which also has an ancestor which is unlocked then its ancestors are not locked" {
+        val node = Node(1, ancestor = Node(2, status = Status.Locked, ancestor = Node(3, status = Status.Unlocked)))
+        node.ancestorsAreLocked() shouldBe false
+    }
+    "when a node has a right which is locked then its right is locked" {
+        val node = Node(1, right = Node(2, status = Status.Locked))
+        node.rightIsLocked() shouldBe true
+    }
+    "when a node has a right which is unlocked then its right is unlocked" {
+        val node = Node(1, right = Node(2, status = Status.Unlocked))
+        node.rightIsLocked() shouldBe false
+    }
+    "when a node has a right which is locked which also has a descendant which is locked then its right is locked" {
+        val node = Node(1, right = Node(2, status = Status.Locked, right = Node(3, status = Status.Locked)))
+        node.rightIsLocked() shouldBe true
+    }
+    "when a node has a right which is locked which also has a descendant which is unlocked then its right are not locked" {
+        val node = Node(1, right = Node(2, status = Status.Locked, right = Node(3, status = Status.Unlocked)))
+        node.rightIsLocked() shouldBe false
+    }
+    "when a node has a left which is locked then its left is locked" {
+        val node = Node(1, left = Node(2, status = Status.Locked))
+        node.leftIsLocked() shouldBe true
+    }
+    "when a node has a left which is unlocked then its left is unlocked" {
+        val node = Node(1, left = Node(2, status = Status.Unlocked))
+        node.leftIsLocked() shouldBe false
+    }
+    "when a node has a left which is locked which also has a descendant which is locked then its left is locked" {
+        val node = Node(1, left = Node(2, status = Status.Locked, left = Node(3, status = Status.Locked)))
+        node.leftIsLocked() shouldBe true
+    }
+    "when a node has a left which is locked which also has a descendant which is unlocked then its left are not locked" {
+        val node = Node(1, left = Node(2, status = Status.Locked, left = Node(3, status = Status.Unlocked)))
+        node.leftIsLocked() shouldBe false
+    }
+    "when a node has an ancestor which is locked and a descendant which is unlocked then ancestors and descendants are not locked" {
+        val node = Node(1, ancestor = Node(2, status = Status.Locked), left= Node(3, status = Status.Unlocked))
+        node.ancestorsAndDescendantsAreLocked() shouldBe false
+    }
+    "when a node has an ancestor which is unlocked and a descendant which is locked then ancestors and descendants are not locked" {
+        val node = Node(1, ancestor = Node(1, status = Status.Unlocked), left= Node(1, status = Status.Locked))
+        node.ancestorsAndDescendantsAreLocked() shouldBe false
+    }
+    "when a node has an ancestor which is locked and a descendant which is locked then ancestors and descendants are not locked" {
+        val node = Node(1, ancestor = Node(1, status = Status.Locked), left= Node(1, status = Status.Locked))
+        node.ancestorsAndDescendantsAreLocked() shouldBe true
+    }
+    "when a node has an ancestor which is unlocked and a descendant which is locked then status cannot be changed to locked" {
+        val node = Node(1, status = Status.Unlocked, ancestor = Node(1, status = Status.Unlocked), left= Node(1, status = Status.Locked))
+        node.lock() shouldBe false
+        node.status shouldBe Status.Unlocked
+    }
+    "when a node has an ancestor which is locked and a descendant which is locked then status can be changed to locked" {
+        val node = Node(1, status = Status.Unlocked, ancestor = Node(1, status = Status.Locked), left= Node(1, status = Status.Locked))
+        node.lock() shouldBe true
+        node.status shouldBe Status.Locked
+    }
+    "when a node has an ancestor which is unlocked and a descendant which is locked then status cannot be changed to unlocked" {
+        val node = Node(1, status = Status.Locked, ancestor = Node(1, status = Status.Unlocked), left= Node(1, status = Status.Locked))
+        node.unlock() shouldBe false
+        node.status shouldBe Status.Locked
+    }
+    "when a node has an ancestor which is locked and a descendant which is locked then status can be changed to unlocked" {
+        val node = Node(1, status = Status.Locked, ancestor = Node(1, status = Status.Locked), left= Node(1, status = Status.Locked))
+        node.unlock() shouldBe true
+        node.status shouldBe Status.Unlocked
+    }
+})
