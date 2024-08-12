b047dc7cbe397f5e3883a1e70f2a4f08f5ef258e
Mike Neilens <mike.neilens@gmail.com>
2024-08-12 18:47:21 +0100

    aug12

--- src/main/kotlin/august12/Problem.kt
+++ src/main/kotlin/august12/Problem.kt
@@ -2,7 +2,7 @@ package august12
 
 //Write an algorithm to justify text. Given a sequence of words and an integer line length k, return a list of strings which represents each line, fully justified.
 //
-//More specifically, you should have as many words as possible in each line. There should be at least one space between each word. Pad extra spaces when necessary so that each line has exactly length k. Spaces should be distributed as equally as possible, with the extra spaces, if any, distributed starting from the left.
+//More specifically, you should have as many words as possible in each line. There should be at least one space between each word. Pad extra spaces when necessary so that each line has exactly length k. Spaces should be distributed as equally possible, with the extra spaces, if any, distributed starting from the left.
 //
 //If you can only fit one word on a line, then you should pad the right-hand side with spaces.
 //
