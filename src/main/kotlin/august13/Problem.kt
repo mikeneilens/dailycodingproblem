6fbae39716a53a5155b595b1ffce5b8aa806917e
Mike Neilens <mike.neilens@gmail.com>
2024-08-13 17:27:42 +0100

    aug13

---
+++ src/main/kotlin/august13/Problem.kt
@@ -0,0 +1,24 @@
+package august13
+
+//Run-length encoding is a fast and simple method of encoding strings.
+// The basic idea is to represent repeated successive characters as a single count and character.
+// For example, the string "AAAABBBCCDAA" would be encoded as "4A3B2C1D2A".
+//
+//Implement run-length encoding and decoding.
+//You can assume the string to be encoded have no digits and consists solely of alphabetic characters.
+//You can assume the string to be decoded is valid.
+
+
+fun problem(string: String) = string.toPairs().encodeAsString()
+
+fun List<Pair<Char, Int>>.encodeAsString() = map{"${it.second}${it.first}"}.joinToString("")
+
+fun String.toPairs():List<Pair<Char, Int>> = fold(listOf()) { pairs, char ->
+    when {
+        pairs.isEmpty() -> listOf(Pair(char, 1))
+        pairs.last().first == char -> pairs.incrementLastPair()
+        else -> pairs + Pair(char, 1)
+    }
+}
+
+fun List<Pair<Char, Int>>.incrementLastPair() = dropLast(1) + Pair(last().first, last().second + 1)
---
+++ src/test/kotlin/august13/ProblemTest.kt
@@ -0,0 +1,22 @@
+package august13
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class ProblemTest: StringSpec({
+    "[Pair<a,4>] encodes to 4a" {
+        listOf(Pair('a',4)).encodeAsString() shouldBe "4a"
+    }
+    "[Pair<a,4>, Pair<12,b>] encodes to 4a12b" {
+        listOf(Pair('a',4), Pair('b',12)).encodeAsString() shouldBe "4a12b"
+    }
+    "aaaa as a list of Pair<Char, Int> is [Pair('a',4)] " {
+        "aaaa".toPairs() shouldBe listOf(Pair('a',4))
+    }
+    "aaaabbb as a list of Pair<Char, Int> is [Pair('a',4),Pair('b',3)] " {
+        "aaaabbb".toPairs() shouldBe listOf(Pair('a',4), Pair('b',3))
+    }
+    "the string AAAABBBCCDAA is encoded as 4A3B2C1D2A" {
+        problem("AAAABBBCCDAA") shouldBe "4A3B2C1D2A"
+    }
+})
