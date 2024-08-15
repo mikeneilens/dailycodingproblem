c26d21caf269d66c8dc668b0af0167566451baa4
Mike Neilens <mike.neilens@gmail.com>
2024-08-15 19:08:37 +0100

    aug15

--- src/main/kotlin/august15/Problem.kt
+++ src/main/kotlin/august15/Problem.kt
@@ -1,6 +1,9 @@
 package august15
 
-//The edit distance between two strings refers to the minimum number of character insertions, deletions, and substitutions required to change one string to the other. For example, the edit distance between “kitten” and “sitting” is three: substitute the “k” for “s”, substitute the “e” for “i”, and append a “g”.
+//The edit distance between two strings refers to the minimum number of character insertions, deletions, and
+// substitutions required to change one string to the other.
+// For example, the edit distance between “kitten” and “sitting” is three:
+// substitute the “k” for “s”, substitute the “e” for “i”, and append a “g”.
 //
 //Given two strings, compute the edit distance between them.
 
@@ -11,21 +14,21 @@ fun Matrix.get(i:Int, j:Int) = get(P(i, j)) ?: 0
 
 fun problem(str1:String, str2: String):Int {
     val matrix  = levenshteinFullMatrix(str1, str2)
-    return matrix[P(str1.length, str2.length)] ?: 0
+    return matrix[P(str1.lastIndex, str2.lastIndex)] ?: 0
 }
 
 fun levenshteinFullMatrix(str1: String, str2: String, matrix: Matrix = mutableMapOf()):Matrix {
-    for (i in 1..str1.length) {
-        for (j in 1..str2.length) {
-            matrix[P(i,j)] =
-                if (str1[i - 1] == str2[j - 1]) { //Characters match
-                    matrix.get(i - 1, j - 1)
+    str1.forEachIndexed  { i, char1 ->
+        str2.forEachIndexed { j, char2 ->
+            matrix[P(i + 1, j + 1)] =
+                if (char1 == char2) { //Characters match
+                    matrix.get(i, j)
                 } else {
-                    (1 + minOf(
-                        matrix.get(i,j - 1),   //Insert
-                        matrix.get(i - 1, j),  //Remove
-                        matrix.get(i - 1,j - 1) // Replace
-                    ))
+                    1 + minOf(
+                        matrix.get(i + 1, j),   //Insert
+                        matrix.get(i,  j + 1),  //Remove
+                        matrix.get(i, j) // Replace
+                    )
                 }
         }
     }
--- src/test/kotlin/august15/ProblemTest.kt
+++ src/test/kotlin/august15/ProblemTest.kt
@@ -6,11 +6,11 @@ import io.kotest.matchers.shouldBe
 class ProblemTest: StringSpec( {
     "edit distance of kitten and sitting is 3" {
         val matrix = levenshteinFullMatrix("kitten", "sitting")
-        matrix.get(6,7) shouldBe 3
+        matrix.get(5,6) shouldBe 3
     }
     "edit distance of sitting and kitten is 3" {
         val matrix = levenshteinFullMatrix("sitting", "kitten")
-        matrix.get(7,6) shouldBe 3
+        matrix.get(6,5) shouldBe 3
     }
     "edit distance of cotton and sitting is 4" {
         problem("cotton","sitting") shouldBe 4
