20a2de9cb0c2da739998c0d2b9c4e58c7b7e4c19
Mike Neilens <mike.neilens@gmail.com>
2024-08-15 18:36:10 +0100

    aug15

---
+++ src/test/kotlin/august15/ProblemTest.kt
@@ -0,0 +1,18 @@
+package august15
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class ProblemTest: StringSpec( {
+    "edit distance of kitten and sitting is 3" {
+        val matrix = levenshteinFullMatrix("kitten", "sitting")
+        matrix.get(6,7) shouldBe 3
+    }
+    "edit distance of sitting and kitten is 3" {
+        val matrix = levenshteinFullMatrix("sitting", "kitten")
+        matrix.get(7,6) shouldBe 3
+    }
+    "edit distance of cotton and sitting is 4" {
+        problem("cotton","sitting") shouldBe 4
+    }
+})
