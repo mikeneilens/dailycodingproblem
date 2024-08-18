51b36dbc14163cb752a369b50f0c2fe04cce4278
Mike Neilens <mike.neilens@gmail.com>
2024-08-18 19:42:29 +0100

    aug17

---
+++ src/main/kotlin/august17/Problem.kt
@@ -0,0 +1,27 @@
+package august17
+
+//Compute the running median of a sequence of numbers. That is, given a stream of numbers, print out the median of the list so far on each new element.
+//
+//Recall that the median of an even-numbered list is the average of the two middle numbers.
+//
+//For example, given the sequence [2, 1, 5, 7, 2, 0, 5], your algorithm should print out:
+//
+//2
+//1.5
+//2
+//3.5
+//2
+//2
+//2
+
+fun problem(list:List<Int>):List<Double> = list.fold(Pair(listOf<Double>(), listOf<Int>())){ output, value ->
+        val newMedian = (output.second + listOf(value)).sorted().median()
+        Pair(output.first + newMedian, output.second + value)
+}.first
+
+fun List<Int>.median():Double {
+    val middleRange = middleRange()
+    return 1.0 * middleRange.sumOf { this[it] } / middleRange.size
+}
+
+fun List<Int>.middleRange() = if (size % 2 == 1) listOf(size/2) else listOf(size/2 - 1, size/2)
---
+++ src/test/kotlin/august17/ProblemTest.kt
@@ -0,0 +1,37 @@
+package august17
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class ProblemTest: StringSpec({
+    "middle range of a list of length 1 is [0]" {
+        listOf(1).middleRange() shouldBe listOf(0)
+    }
+    "middle range of a list of length 2 is [0, 1]" {
+        listOf(1,2).middleRange() shouldBe listOf(0, 1)
+    }
+    "middle range of a list of length 3 is [1]" {
+        listOf(1,2,3).middleRange() shouldBe listOf(1)
+    }
+    "middle range of a list of length 4 is [1..2]" {
+        listOf(1,2,3,4).middleRange() shouldBe listOf(1, 2)
+    }
+    "median of [2] is 2" {
+        listOf(2).median() shouldBe 2
+    }
+    "median of [1, 2] is 1.5" {
+        listOf(1, 2).median() shouldBe 1.5
+    }
+    "median of [1, 2, 5] is 2" {
+        listOf(1, 2, 5).median() shouldBe 2
+    }
+    "with [2] the problem returns [2]" {
+        problem(listOf(2)) shouldBe listOf(2)
+    }
+    "with [2, 1] the problem returns [2, 1.5]" {
+        problem(listOf(2, 1)) shouldBe listOf(2, 1.5)
+    }
+    "with [2, 1, 5, 7, 2, 0, 5] the problem returns [2, 1.5, 2, 3.5, 2, 2, 2]" {
+        problem(listOf(2, 1, 5, 7, 2, 0, 5)) shouldBe listOf(2, 1.5, 2, 3.5, 2, 2, 2)
+    }
+})
