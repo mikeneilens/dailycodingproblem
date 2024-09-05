f86c18211d5b211dec975dfe7c3a8627a6f973f9
Mike Neilens <mike.neilens@gmail.com>
2024-09-05 18:11:36 +0100

    sep05

---
+++ src/main/kotlin/september/september05/Problem.kt
@@ -0,0 +1,42 @@
+package september.september05
+
+//Implement an LRU (Least Recently Used) cache. It should be able to be initialized with a cache size n, and
+// contain the following methods:
+//
+//set(key, value): sets key to value. If there are already n items in the cache and we are adding a new item,
+// then it should also remove the least recently used item.
+//get(key): gets the value at key. If no such key exists, return null.
+//Each operation should run in O(1) time.
+
+data class Item(var itemkey:String, var next:Item? = null)
+
+fun createItems(n:Int, start:Item = Item("$n"), current:Item = start):Item {
+    return if (n == 1) {
+        current.next = start
+        start
+    } else {
+        Item("${n - 1}").let{ newItem ->
+            current.next = newItem
+            createItems(n - 1, start, newItem)
+        }
+    }
+}
+
+data class Cache(val n:Int) {
+    var oldestItems:Item
+    val cacheMap:MutableMap<String, Int> = mutableMapOf()
+
+    fun set(key:String, value:Int) {
+        if (cacheMap[key] != value) {
+            cacheMap.remove(oldestItems.itemkey)
+            cacheMap[key] = value
+            oldestItems.itemkey = key
+            oldestItems = oldestItems?.next ?: Item("")
+        }
+    }
+    fun get(key:String) = cacheMap[key]
+
+    init {
+        oldestItems = createItems(n)
+    }
+}
---
+++ src/test/kotlin/september/september05/ProblemTest.kt
@@ -0,0 +1,61 @@
+package september.september05
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class ProblemTest: StringSpec( {
+    "initialising oldest items with size 5 producing circular list of 5 items" {
+        val items = createItems(5)
+        items.itemkey shouldBe "5"
+        items.next?.itemkey shouldBe "4"
+        items.next?.next?.itemkey shouldBe "3"
+        items.next?.next?.next?.itemkey shouldBe "2"
+        items.next?.next?.next?.next?.itemkey shouldBe "1"
+        items.next?.next?.next?.next?.next?.itemkey shouldBe "5"
+    }
+    "creating a cache of size 3 creates a cache with oldestItems of size 3" {
+        val cache = Cache(3)
+        cache.oldestItems.itemkey = "3"
+        cache.oldestItems.next?.itemkey = "2"
+        cache.oldestItems.next?.next?.itemkey = "1"
+        cache.oldestItems.next?.next?.next?.itemkey = "3"
+    }
+    "create a cache of size 3 and add an item to it and retrieve it" {
+        val cache = Cache(3)
+        cache.set("item1", 1)
+        cache.get("item1") shouldBe 1
+    }
+    "create a cache of size 3 and add 3 items to it and retrieve them" {
+        val cache = Cache(3)
+        cache.set("item1", 1)
+        cache.set("item2", 2)
+        cache.set("item3", 3)
+        cache.get("item1") shouldBe 1
+        cache.get("item2") shouldBe 2
+        cache.get("item3") shouldBe 3
+        cache.cacheMap.size shouldBe  3
+    }
+    "create a cache of size 3 and add 4 items to it and retrieve them" {
+        val cache = Cache(3)
+        cache.set("item1", 1)
+        cache.set("item2", 2)
+        cache.set("item3", 3)
+        cache.set("item4", 4)
+        cache.get("item1") shouldBe null
+        cache.get("item2") shouldBe 2
+        cache.get("item3") shouldBe 3
+        cache.get("item4") shouldBe 4
+        cache.cacheMap.size shouldBe  3
+    }
+    "create a cache of size 3 and add 4 items to it, including a duplicate and retrieve them" {
+        val cache = Cache(3)
+        cache.set("item1", 1)
+        cache.set("item2", 2)
+        cache.set("item2", 2)
+        cache.set("item3", 3)
+        cache.get("item1") shouldBe 1
+        cache.get("item2") shouldBe 2
+        cache.get("item3") shouldBe 3
+        cache.cacheMap.size shouldBe  3
+    }
+})
