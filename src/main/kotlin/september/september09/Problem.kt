f87459528a030b6c7146a74e0981d7dfd250da75
Mike Neilens <mike.neilens@gmail.com>
2024-09-09 19:31:32 +0100

    sep09

---
+++ src/main/kotlin/september/september09/Problem.kt
@@ -0,0 +1,22 @@
+package september.september09
+
+//Given an undirected graph represented as an adjacency matrix and an integer k,
+// write a function to determine whether each vertex in the graph can be colored such that no two adjacent vertices
+// share the same color using at most k colors.
+
+//Each row and column indicates a vertexNo and TRUE at [row][col] indicates vertex #row is a neighbour of vertex #col
+typealias Vertexes = List<List<Boolean>>
+
+//Does a depth first search, not optimised, so it returns lots of results instead of stopping after the first
+fun addColour(vertexNo:Int, coloursForEachVertex:List<String> = listOf(), vertexes:Vertexes, colours:Set<String>):List<List<String>> =
+    if (vertexNo >= vertexes.size)
+        listOf(coloursForEachVertex)
+    else
+        candidateColours(vertexNo, coloursForEachVertex, vertexes,colours)
+        .flatMap {colour ->  addColour(vertexNo + 1, coloursForEachVertex + colour, vertexes, colours) }
+
+fun candidateColours(vertexNo:Int, coloursForEachVertex: List<String>, vertexes: Vertexes, colours: Set<String> ):Set<String> {
+    val neighbourColours = vertexes[vertexNo].mapIndexed { index, isNeighbour ->
+        if(isNeighbour && index < coloursForEachVertex.size ) coloursForEachVertex[index] else "" }.toSet()
+    return colours - neighbourColours
+}
---
+++ src/test/kotlin/september/september09/ProblemTest.kt
@@ -0,0 +1,71 @@
+package september.september09
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class ProblemTest: StringSpec({
+    "if three vertexes and three colours and processing first vertex there are three candidate colours" {
+        val vertexes = listOf(
+            listOf(false, true, true),
+            listOf(true, false, true),
+            listOf(true, true, false)
+        )
+        val colours = setOf("red","green","blue")
+
+        candidateColours(0, listOf(),vertexes, colours) shouldBe setOf("red","green","blue")
+    }
+    "if three vertexes and three colours and processing second vertex there are two candidate colours" {
+        val vertexes = listOf(
+            listOf(false, true, true),
+            listOf(true, false, true),
+            listOf(true, true, false)
+        )
+        val colours = setOf("red","green","blue")
+
+        candidateColours(1, listOf("red"),vertexes, colours) shouldBe setOf("green","blue")
+    }
+
+    "if three vertexes and three colours and processing third vertex there is one candidate colour" {
+        val vertexes = listOf(
+            listOf(false, true, true),
+            listOf(true, false, true),
+            listOf(true, true, false)
+        )
+        val colours = setOf("red","green","blue")
+
+        candidateColours(2, listOf("red","blue"),vertexes, colours) shouldBe setOf("green")
+    }
+    "if three vertexes and three colours there is a possible combination of colours" {
+        val vertexes = listOf(
+            listOf(false, true, true),
+            listOf(true, false, true),
+            listOf(true, true, false)
+        )
+        val colours = setOf("red","green","blue")
+
+        addColour(0, listOf(),vertexes, colours).map{it.toSet()}.distinct() shouldBe
+                listOf( setOf("red","green","blue") )
+    }
+    "if three vertexes and two colours there is no possible combination of colours" {
+        val vertexes = listOf(
+            listOf(false, true, true),
+            listOf(true, false, true),
+            listOf(true, true, false)
+        )
+        val colours = setOf("red","green")
+
+        addColour(0, listOf(),vertexes, colours).map{it.toSet()}.distinct() shouldBe listOf()
+    }
+    "with four vertexes and three colours there is at least one possible combination of colours" {
+        val vertexes = listOf(
+            listOf(false, true, true, true),
+            listOf(true, false, true, false),
+            listOf(true, true, false, true),
+            listOf(true, false, true, false)
+        )
+
+        val colours = setOf("red","green","blue")
+
+        addColour(0, listOf(),vertexes, colours).first() shouldBe listOf("red", "green", "blue", "green")
+    }
+})
