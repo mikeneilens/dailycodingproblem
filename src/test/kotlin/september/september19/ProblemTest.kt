9d046802ffe8bd2308c0d1b50409a94c5800b55f
Mike Neilens <mike.neilens@gmail.com>
2024-09-19 18:33:25 +0100

    sept19

--- src/test/kotlin/september/september19/ProblemTest.kt
+++ src/test/kotlin/september/september19/ProblemTest.kt
@@ -13,12 +13,12 @@ class ProblemTest: StringSpec({
     }
     "unbiased random gives heads if random number sequence is always 1,0" {
         var n = 0
-        val biasedRandom = { listOf(1,0).get(n++).also { n = n % 2 } }
+        val biasedRandom = listOf(1,0).fixedRandomGenerator()
         (1..1000).map{unBiasedCoin(biasedRandom)}.all{it == "heads"} shouldBe true
     }
     "unbiased random gives tails if random number sequence is always 0,1" {
         var n = 0
-        val biasedRandom = { listOf(0,1).get(n++).also { n = n % 2 } }
+        val biasedRandom = listOf(0,1).fixedRandomGenerator()
         (1..1000).map{unBiasedCoin(biasedRandom)}.all{it == "tails"} shouldBe true
     }
     "test unbiased random coin" {
@@ -28,5 +28,9 @@ class ProblemTest: StringSpec({
         ratioOfHeads shouldBeGreaterThan 0.49
         ratioOfHeads shouldBeLessThan 0.51
     }
-
 })
+
+fun List<Int>.fixedRandomGenerator():()->Int {
+    var n = 0
+    return { get(n++).also { n = n % size } }
+}
