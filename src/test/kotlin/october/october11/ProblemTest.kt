40e2461dc03c1fd56ebe0c9666e2bf2f24db4310
Mike Neilens <mike.neilens@gmail.com>
2024-10-13 11:40:34 +0100

    oct11

--- src/test/kotlin/october/october11/ProblemTest.kt
+++ src/test/kotlin/october/october11/ProblemTest.kt
@@ -6,24 +6,24 @@ import io.kotest.matchers.shouldBe
 
 class ProblemTest: StringSpec({
     "dividing 0 by 2 gives 0" {
-        (0 div 2) shouldBe 0
+        0 div 2 shouldBe 0
     }
     "dividing 5 by 2 gives 2" {
-        (5 div 2) shouldBe 2
+        5 div 2 shouldBe 2
     }
     "dividing 100 by 3 gives 33" {
-        (100 div 3) shouldBe 33
+        100 div 3 shouldBe 33
     }
     "dividing 100 by -3 gives -33" {
-        (100 div -3) shouldBe  -33
+        100 div -3 shouldBe  -33
     }
     "dividing -100 by 3 gives -33" {
-        (-100 div 3) shouldBe  -33
+        -100 div 3 shouldBe  -33
     }
     "dividing -100 by -3 gives 33" {
-        (-100 div -3) shouldBe  33
+        -100 div -3 shouldBe  33
     }
-    "dividig 1 by 0 throws an error" {
+    "dividing 1 by 0 throws an error" {
         shouldThrowExactly<ArithmeticException> {
             1 div 0
         }
