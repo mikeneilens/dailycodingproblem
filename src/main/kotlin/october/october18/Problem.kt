d077255194daf8f02f7290e4fd490c1fbe98222e
Mike Neilens <mike.neilens@gmail.com>
2024-10-20 19:59:32 +0100

    oct18 outline algorith only

---
+++ src/main/kotlin/october/october18/Problem.kt
@@ -0,0 +1,16 @@
+package october.october18
+
+//Given a number represented by a list of digits, find the next greater permutation of a number,
+// in terms of lexicographic ordering.
+// If there is not greater permutation possible, return the permutation with the lowest value/ordering.
+//
+//For example, the list [1,2,3] should return [1,3,2]. The list [1,3,2] should return [2,1,3].
+// The list [3,2,1] should return [1,2,3].
+
+
+
+//With an array of numbers, determine position of last ascending digit and call it p
+
+//With an array of numbers, determine position of last digit greater than [p]
+
+//With an array of numbers, if there is more than 1 digit after position p, reverse the digits after position p
---
+++ src/test/kotlin/october/october18/ProblemTest.kt
@@ -0,0 +1,18 @@
+package october.october18
+
+import io.kotest.core.spec.style.StringSpec
+
+class ProblemTest: StringSpec( {
+    "With an array [1,2,3,4], the position of the last ascending digit is 2" {
+
+    }
+    "With an array [4,3,2,1], the position of the last ascending digit is null" {
+
+    }
+    "With an array [1,2,3,4], the position of the last digit greater then [2] is 3" {
+
+    }
+    "With an array [1,3,4,2], reversing the digits after position 1 gives [1,3,2,4] " {
+
+    }
+})
