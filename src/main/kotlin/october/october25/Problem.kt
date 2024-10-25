054e62a6d4585687248c51b486c88d15106d714c
Mike Neilens <mike.neilens@gmail.com>
2024-10-25 18:59:03 +0100

    oct25

--- src/main/kotlin/october/october25/Problem.kt
+++ src/main/kotlin/october/october25/Problem.kt
@@ -4,8 +4,6 @@ package october.october25
 //
 //For example, if the list is [1, 2, 3, 4, 5] and K is 9, then it should return [2, 3, 4], since 2 + 3 + 4 = 9.
 
-
-
 fun problem(numbers:List<Int>, target:Int):List<List<Int>> =
     numbers.createListOfSubLists()
         .flatMap{it.leadingSubListWithTotalEqualTo(target)}
