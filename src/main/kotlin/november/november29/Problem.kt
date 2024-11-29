c9bff1e0de86252130da00e8fc4e568d8ed8ab77
Mike Neilens <mike.neilens@gmail.com>
2024-11-29 20:10:17 +0000

    nov29

--- src/main/kotlin/november/november29/Problem.kt
+++ src/main/kotlin/november/november29/Problem.kt
@@ -1,2 +1,39 @@
 package november.november29
 
+//Given an N by M matrix consisting only of 1's and 0's, find the largest rectangle containing only 1's and return its area.
+//
+//For example, given the following matrix:
+//
+//[[1, 0, 0, 0],
+// [1, 0, 1, 1],
+// [1, 0, 1, 1],
+// [0, 1, 0, 0]]
+
+typealias Matrix = List<List<Int>>
+
+fun problem(matrix:Matrix) =
+    matrix.indices.flatMap{row -> matrix.first().indices.map{col -> matrix.maxAt(row, col)}}.maxBy { it.area()}
+
+fun Matrix.maxAt(row:Int, col:Int):Pair<IntRange, IntRange> =
+    rangesFor(row, col)
+        .fold(Pair(0..0, 0..0)){ bestRanges, ranges ->
+            if (containsOnlyOnes(ranges.first, ranges.second) && ranges.area() > bestRanges.area())
+                ranges
+            else bestRanges
+        }
+
+fun Matrix.rangesFor(row:Int, col:Int) =
+    (col until (col + width(row, col) )).flatMap { dc ->
+        (row until (row + depth(row, col))).map { dr ->
+            Pair(row..dr, col..dc) } }
+
+fun Matrix.containsOnlyOnes(rowRange:IntRange, colRange:IntRange) =
+    rowRange.flatMap { row ->  colRange.map{col -> this[row][col] }}.all{it == 1}
+
+fun Matrix.width(row:Int, col:Int) =
+    this[row].drop(col).takeWhile { it == 1 }.size
+
+fun Matrix.depth(row:Int, col:Int) =
+    drop(row).map{it[col]}.takeWhile { it == 1 }.size
+
+fun Pair<IntRange, IntRange>.area() = (first.last - first.first + 1) * (second.last - second.first + 1)
--- src/test/kotlin/november/november29/ProblemTest.kt
+++ src/test/kotlin/november/november29/ProblemTest.kt
@@ -1,4 +1,80 @@
 package november.november29
 
-class ProblemTest {
-}
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class ProblemTest: StringSpec({
+    val testData = listOf(
+        listOf(1,0,0,0),
+        listOf(1,0,1,1),
+        listOf(1,0,1,1),
+        listOf(0,1,0,0)
+    )
+    "width starting at col 2 of [[0,0,1,1,1,0]]is 3 " {
+        listOf(listOf(0,0,1,1,1,0,0)).width(0,2) shouldBe 3
+    }
+    "width starting at col 2 of [[0,0,0,1,1,0]]is 0 " {
+        listOf(listOf(0,0,0,1,1,0,0)).width(0,2) shouldBe 0
+    }
+    "depth starting at row 1 of [[0,0,0],[1,0,0],[1,0,0],[0,0,0] is 2]" {
+        listOf(listOf(0,0,0),listOf(1,0,0),listOf(1,0,0),listOf(0,0,0)).depth(1,0) shouldBe 2
+    }
+    "depth starting at row 3 of [[0,0,0],[1,0,0],[1,0,0],[0,0,0] is 0]" {
+        listOf(listOf(0,0,0),listOf(1,0,0),listOf(1,0,0),listOf(0,0,0)).depth(3,0) shouldBe 0
+    }
+    " at row 1..3 and col 1..2 [[0,0,0,0],[0,1,1,0],[0,1,1,0],[0,1,1,0],[0,0,0,0]] should contain only 1s" {
+        listOf(
+            listOf(0,0,0,0),
+            listOf(0,1,1,0),
+            listOf(0,1,1,0),
+            listOf(0,1,1,0),
+            listOf(0,0,0,0),
+        ).containsOnlyOnes(1..3, 1..2) shouldBe true
+    }
+    " at row 1..4 and col 1..2 [[0,0,0,0],[0,1,1,0],[0,1,1,0],[0,1,1,0],[0,0,0,0]] should not contain only 1s" {
+        listOf(
+            listOf(0,0,0,0),
+            listOf(0,1,1,0),
+            listOf(0,1,1,0),
+            listOf(0,1,1,0),
+            listOf(0,0,0,0),
+        ).containsOnlyOnes(1..4, 1..2) shouldBe false
+    }
+    " at row 1..3 and col 1..1 [[0,0,0,0],[0,1,1,0],[0,1,1,0],[0,1,1,0],[0,0,0,0]] should contain only 1s" {
+        listOf(
+            listOf(0,0,0,0),
+            listOf(0,1,1,0),
+            listOf(0,1,1,0),
+            listOf(0,1,1,0),
+            listOf(0,0,0,0),
+        ).containsOnlyOnes(1..3, 1..1) shouldBe true
+    }
+    " at row 1..3 and col 2..2 [[0,0,0,0],[0,1,1,0],[0,1,1,0],[0,1,1,0],[0,0,0,0]] should contain only 1s" {
+        listOf(
+            listOf(0,0,0,0),
+            listOf(0,1,1,0),
+            listOf(0,1,1,0),
+            listOf(0,1,1,0),
+            listOf(0,0,0,0),
+        ).containsOnlyOnes(1..3, 2..2) shouldBe true
+    }
+    "with test data, max grid at 0,0 is (0..2, 0..0)" {
+        testData.maxAt(0,0) shouldBe Pair(0..2, 0..0)
+    }
+    "with test data, max grid at 1,2 is (1..2, 2..3)" {
+        testData.maxAt(1,2) shouldBe Pair(1..2, 2..3)
+    }
+    "with the test matrix the max is (1..2, 2..3)" {
+        problem(testData) shouldBe Pair(1..2, 2..3)
+    }
+    "with [[0,0,0,0],[0,1,1,0],[0,1,1,0],[0,1,1,0],[0,0,0,0]] the max is (1..3,1..2)" {
+        val matrix = listOf(
+            listOf(0,0,0,0),
+            listOf(0,1,1,0),
+            listOf(0,1,1,0),
+            listOf(0,1,1,0),
+            listOf(0,0,0,0),
+        )
+        problem(matrix) shouldBe Pair(1..3, 1..2)
+    }
+})
